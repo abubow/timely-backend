be3c5626187686ed4c51d82064c65250780e9c84
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-05-28 14:57:16 +0500

    hello world request added

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
+func hello(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello World!",
+	})
+}
 func main() {
 	fmt.Println("Hello World!")
+	r := gin.Default()
+	// on /hello path
+	r.GET("/hello", hello)
+	r.Run() // listen and serve on
 }
